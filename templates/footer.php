af169d74be074d3c11bd6a0e8479addcbe98706c
Eric Ryan Harrison <me@ericharrison.info>
2023-10-03 00:09:37 -0400

    Update footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,14 +2,29 @@
         <footer class="py-5 bg-dark">
             <div class="container px-5">
 				<div class="row">
-					<p class="mb-3 text-end">
-						<code class="text-light">Page generated in: <span class="text-info"><?= sprintf('%0.4f', (microtime(true) - $__painfree_start_time)) . 's'; ?></span> 🔥🔥</code>
-					</p>
-				</div>
-				<div class="row">
-					<p class="m-0 text-center text-white">
-						Copyright &copy;<?= date('Y'); ?> <a href="https://github.com/Programming-is-Easy/PHPainfree">PHPainfree</a>
-					</p>
+					<div class="col-4">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link p-0 pb-1" href="https://github.com/Programming-is-Easy/PHPainfree/blob/main/LICENSE">BSD-3 License</a>	
+							</li>
+							<li class="nav-item">
+								<a class="nav-link p-0 pb-1" href="">Contribute</a>	
+							</li>
+							<li class="nav-item">
+								<a class="nav-link p-0 pb-1" href="https://programming-is-easy.com">Programming is Easy</a>	
+							</li>
+						</ul>
+					</div>
+					<div class="col-4">
+						<p class="m-0 text-center text-white">
+							Copyright &copy;<?= date('Y'); ?> <a href="https://github.com/Programming-is-Easy/PHPainfree">PHPainfree</a>
+						</p>
+					</div>
+					<div class="col-4">
+						<p class="mb-3 text-end">
+							<code class="text-light">Page generated in: <span class="text-info"><?= sprintf('%0.4f', (microtime(true) - $__painfree_start_time)) . 's'; ?></span> 🔥🔥</code>
+						</p>
+					</div>
 				</div>
 			</div>
         </footer>
